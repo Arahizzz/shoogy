2acd4a536bdf9ff50b92c173e01b75e3c72b768a
Yurii Polishchuk <yuriipolishchuk1@gmail.com>
2024-05-23 00:53:28 +0300

    IOB and COB indicators

--- app/(app)/(tabs)/activities/index.tsx
+++ app/(app)/(tabs)/activities/index.tsx
@@ -17,7 +17,7 @@ import {
 } from '@tamagui/lucide-icons';
 import { IconProps } from '@tamagui/helpers-icon';
 import IndexActivityChart from '~/components/index-activity-chart';
-import { currentSugarValue$ } from '~/core/chart/data';
+import { currentCob$, currentIob$, currentSugarValue$ } from '~/core/calculations/data';
 
 export default function CombinedScreen() {
   useSubscription(glucoseFetchWorker);
@@ -39,22 +39,28 @@ function CurrentGlucose() {
   if (!currentGlucose) return <></>;
 
   return (
-    <XStack gap={10} alignItems={'stretch'}>
-      <YStack justifyContent={'center'}>
-        <GlucoseDirection direction={currentGlucose.direction} />
-      </YStack>
-      <Text color={'black'} fontSize={80}>
-        {currentGlucose.sugar.toFixed(1)}
-      </Text>
-      <YStack justifyContent={'space-around'}>
-        <Text color={'black'} fontSize={24}>
-          {format(currentGlucose.date, 'HH:mm')}
+    <YStack>
+      <XStack gap={10} alignItems={'stretch'}>
+        <YStack justifyContent={'center'}>
+          <GlucoseDirection direction={currentGlucose.direction} />
+        </YStack>
+        <Text color={'black'} fontSize={80}>
+          {currentGlucose.sugar.toFixed(1)}
         </Text>
-        <Text color={'black'} fontSize={24}>
-          mmol/L
-        </Text>
-      </YStack>
-    </XStack>
+        <YStack justifyContent={'space-around'}>
+          <Text color={'black'} fontSize={24}>
+            {format(currentGlucose.date, 'HH:mm')}
+          </Text>
+          <Text color={'black'} fontSize={24}>
+            mmol/L
+          </Text>
+        </YStack>
+      </XStack>
+      <XStack justifyContent={'space-between'}>
+        <IobInfo />
+        <CobInfo />
+      </XStack>
+    </YStack>
   );
 }
 
@@ -70,4 +76,28 @@ function GlucoseDirection({ direction }: { direction: Direction }) {
     .with(Direction.DoubleUp, () => <ArrowBigUpDash {...iconProps} />)
     .with(Direction.Unknown, () => <ShieldQuestion {...iconProps} />)
     .exhaustive();
+}
+
+function IobInfo() {
+  const [iob] = useObservableState(() => currentIob$, undefined);
+
+  if (!iob) return <></>;
+
+  return (
+    <YStack>
+      <Text color={'black'}>IOB: {iob.toFixed(2)}U</Text>
+    </YStack>
+  );
+}
+
+function CobInfo() {
+  const [cob] = useObservableState(() => currentCob$, undefined);
+
+  if (!cob) return <></>;
+
+  return (
+    <YStack>
+      <Text color={'black'}>COB: {cob.toFixed(2)}g</Text>
+    </YStack>
+  );
 }
