fc7df097094de2e184aa6b83ac734a60546045f7
Yurii Polishchuk <yuriipolishchuk1@gmail.com>
2024-05-23 19:20:58 +0300

    Style login screen

--- app/login.tsx
+++ app/login.tsx
@@ -1,4 +1,4 @@
-import { Button, Input, Text, YStack } from 'tamagui';
+import { Button, Input, Label, Text, XStack, YStack } from 'tamagui';
 import React, { useState } from 'react';
 import loginManager from '~/core/nightscout/login-manager';
 import { router, Stack } from 'expo-router';
@@ -10,6 +10,7 @@ export default function LoginScreen() {
   const onSubmit = async () => {
     const response = await fetch(`${url}/v2/authorization/request/${token}`);
     if (!response.ok) {
+      console.error(response);
       alert('Unable to login');
       return;
     }
@@ -18,11 +19,29 @@ export default function LoginScreen() {
   };
 
   return (
-    <YStack>
-      <Text>Login</Text>
-      <Input inputMode={'url'} value={url} onChangeText={setUrl} placeholder={'URL'} />
-      <Input value={token} onChangeText={setToken} placeholder={'Token'} />
-      <Button onPress={onSubmit}>Submit</Button>
+    <YStack
+      maxWidth={'600px'}
+      height={'100%'}
+      gap={5}
+      alignSelf={'center'}
+      justifyContent={'center'}>
+      <Text fontSize={50} color={'black'} alignSelf={'center'}>
+        Login
+      </Text>
+      <XStack gap={5} justifyContent={'space-between'}>
+        <Label color={'black'}>Nightscout URL:</Label>
+        <Input
+          inputMode={'url'}
+          value={url}
+          onChangeText={setUrl}
+          placeholder={'https://nightscout.com/api'}
+        />
+      </XStack>
+      <XStack gap={5} justifyContent={'space-between'}>
+        <Label color={'black'}>Token:</Label>
+        <Input value={token} onChangeText={setToken} placeholder={'Token'} />
+      </XStack>
+      <Button onPress={onSubmit}>Enter</Button>
     </YStack>
   );
 }
